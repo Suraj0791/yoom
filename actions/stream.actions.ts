01e9cedd0f59c5e465cddbd8848bb27a901bc934
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2025-09-19 16:33:06 +0530

    feat : add stream actions file for verifying token

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,24 +1,24 @@
 'use server';
 
 import { currentUser } from '@clerk/nextjs/server';
-import { StreamChat } from 'stream-chat';
+import { StreamClient } from '@stream-io/node-sdk';
 
 const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-const STREAM_SECRET_KEY = process.env.STREAM_SECRET_KEY;
+const STREAM_API_SECRET = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async () => {
-    const user = await currentUser();
+  const user = await currentUser();
 
-    if (!user) throw new Error('User is not authenticated');
-    if (!STREAM_API_KEY) throw new Error('Stream API key secret is missing');
-    if (!STREAM_SECRET_KEY) throw new Error('Stream secret key is missing');
+  if (!user) throw new Error('User is not authenticated');
+  if (!STREAM_API_KEY) throw new Error('Stream API key secret is missing');
+  if (!STREAM_API_SECRET) throw new Error('Stream API secret is missing');
 
-    const streamChat = StreamChat.getInstance(STREAM_API_KEY, STREAM_SECRET_KEY);
+  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
 
-    const expirationTime = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
-    const issuedAt = Math.floor(Date.now() / 1000) - 60; // 1 minute ago
+  const expirationTime = Math.floor(Date.now() / 1000) + 3600;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
 
-    const token = streamChat.createToken(user.id, expirationTime, issuedAt);
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
 
-    return token;
+  return token;
 };
